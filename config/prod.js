f8afc6addb759efca4dca1fb3fcb1d2daa7dca8b
Neftali Forero <neftaiof@gmail.com>
2017-10-21 15:37:50 -0400

    Production version

--- config/prod.js
+++ config/prod.js
@@ -11,8 +11,10 @@ config.logs.streams = [
     }
 ];
 config.uploads.folder = '/uploads';
-config.uploads.baseUrl = 'elparaisorojo.com/files';
-config.storefront.baseUrl = 'http://elparaisorojo.com';
+config.uploads.baseUrl = '0.0.0.0:3000/files';
+//config.uploads.baseUrl = 'elparaisorojo.com/files';
+config.storefront.baseUrl = '0.0.0.0:3000';
+//config.storefront.baseUrl = 'http://elparaisorojo.com';
 config.switchPayments.baseUrl = 'https://api.switchpayments.com/v2';
 
 // Export
